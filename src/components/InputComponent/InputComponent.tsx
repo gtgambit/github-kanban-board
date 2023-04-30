a49ea8a6f9542926a462438b3085cb7693975faa
gtgambit <gtgambit@gmail.com>
2023-04-30 15:28:33 +0300

    adjustments for mobile version

--- src/components/InputComponent/InputComponent.tsx
+++ src/components/InputComponent/InputComponent.tsx
@@ -1,7 +1,7 @@
-import React, { FC, useState, ChangeEvent } from "react";
-import { Input, Button } from "antd";
+import { Input, Button, Col, Row } from "antd";
 import { InputProps } from "antd/lib/input";
 import { NotificationError } from "../NotificationError/NotificationError";
+import React, { FC, useState, ChangeEvent } from "react";
 
 interface InputComponentProps extends InputProps {
   inputValue: string;
@@ -36,30 +36,33 @@ export const InputComponent: FC<InputComponentProps> = ({
   return (
     <>
       {errorMessage && <NotificationError description={errorMessage} />}
-      <div style={{ marginBottom: "16px", marginTop: "16px", display: "flex" }}>
-        <Input
-          id="input-id"
-          name="input-name"
-          placeholder="Enter repo URL"
-          value={inputValue}
-          onChange={handleChange}
-          required
-          minLength={3}
-          maxLength={100}
-          pattern="[A-Za-z]+"
-          title={errorMessage}
-          style={{ marginBottom: "8px" }}
-          {...props}
-        />
-
-        <Button
-          type="primary"
-          onClick={handleSubmit}
-          style={{ marginLeft: "8px" }}
-          disabled={isDisabled}>
-          Load Issue
-        </Button>
-      </div>
+      <Row gutter={16} style={{ marginBottom: "16px", marginTop: "16px" }}>
+        <Col xs={24} sm={16}>
+          <Input
+            id="input-id"
+            name="input-name"
+            placeholder="Enter repo URL"
+            value={inputValue}
+            onChange={handleChange}
+            required
+            minLength={3}
+            maxLength={100}
+            pattern="[A-Za-z]+"
+            title={errorMessage}
+            style={{ marginBottom: "8px", padding: "12px" }}
+            {...props}
+          />
+        </Col>
+        <Col xs={24} sm={8}>
+          <Button
+            type="primary"
+            onClick={handleSubmit}
+            style={{ marginLeft: "8px", padding: "12px", width: "100%" }}
+            disabled={isDisabled}>
+            Load Issue
+          </Button>
+        </Col>
+      </Row>
     </>
   );
 };
